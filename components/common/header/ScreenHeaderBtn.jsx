9f917849c21feff2a94c3a4d6ca12f00ca37a771
FATEH BEDDIAF <62912697+madjed-hue@users.noreply.github.com>
2023-04-14 23:55:43 +0100

    first commit

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./screenheader.style";
+import { Image, TouchableOpacity } from "react-native";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+  return (
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default ScreenHeaderBtn;
